4dcd9d48a25506a205dea8abf48e19836d3a65a7
Lucas Tort <92398144+LucasTortuwu@users.noreply.github.com>
2021-10-21 23:17:44 -0300

    Update Dou
    asdasdasd

--- El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/CorePantalla.java
+++ El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/CorePantalla.java
@@ -1,9 +1,13 @@
 package com.mycompany.el.pistolero.Client;
+import com.mycompany.el.pistolero.Client.Pantalla;
+
 public class CorePantalla {
 
 	public static void main(String[] args) {
-		Pantalla pant1 = new Pantalla(400,500);
+		Pantalla pant1 = new Pantalla(400,300);
 		pant1.setVisible(true);
+		
+		
 
 	}
 
--- El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/Pantalla.java
+++ El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/Pantalla.java
@@ -2,19 +2,26 @@ package com.mycompany.el.pistolero.Client;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.event.*;
 public class Pantalla extends JFrame{
 	JButton boton;
 	JLabel label1;
+	String texto;
+	private JTextField campo1;
 	Pantalla (int largo, int ancho) {
 		
-		this.setTitle("SaludarXD");
+		this.setTitle("Titulo de ejemplo");
 		
 		this.setSize(largo, ancho);
 		this.setLayout(null);
 		
-		
-		label1 = new JLabel("Login test");
+		campo1 = new JTextField();
+		campo1.setBounds(110,50,150,30);
+		this.add(campo1);
+		texto= campo1.getText();
+//text
+		label1 = new JLabel(texto);
 		label1.setBounds(110,10,500,30);
 		this.add(label1);
 //label
