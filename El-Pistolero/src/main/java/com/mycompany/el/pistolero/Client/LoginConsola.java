a69b1d4a2dd7b21e666573d961177c95d8445e4d
faustorm07 <faustor318@gmail.com>
2021-11-03 01:40:48 -0300

    Termine
    Si esta listo

--- El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/LoginConsola.java
+++ El-Pistolero/src/main/java/com/mycompany/el/pistolero/Client/LoginConsola.java
@@ -14,23 +14,30 @@ import com.mycompany.el.pistolero.Server.Usuario;
 import java.io.*;
 import java.net.*;
 public class LoginConsola {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InterruptedException{
            ServerSocket serverSocket = new ServerSocket(6666);
        Socket clientSocket = serverSocket.accept();
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         Scanner entrada = new Scanner (System.in);
 while (true){
-pantalla(out,in,entrada);
+                    System.out.println("Bienvenido, que le gustaria hacer a continuacion?");
+                    System.out.println("1:Ingresar, 2:Registrarse");
+                            String eleccion = entrada.next();
+                    if(eleccion.equals("1")){
+                        pantlog(out,in,entrada);
+                    }
+                    else if (eleccion.equals("2")){
+                    pantreg(entrada,out);
+                    }
+                    else{System.out.println("Eleccion incorrecta,porfavor ingrese una opcion valida");}
 }
     
-    }
-    static void pantalla(PrintWriter out,BufferedReader in,Scanner entrada)throws IOException{
-                System.out.println("Bienvenido, que le gustaria hacer a continuacion?");
-                    System.out.println("1:Ingresar, 2:Registrarse");
-        String eleccion = entrada.next();
-    if(eleccion.equals("1")){
-        System.out.println("Nombre de usuario:");
+}
+
+     void pantlog(PrintWriter out,BufferedReader in,Scanner entrada) throws IOException, InterruptedException{
+        String eleccion = "1";
+            System.out.println("Nombre de usuario:");
             String nombreusuarioscn = entrada.next();
                 msj(out,nombreusuarioscn);
         System.out.println("Contraseña:");
@@ -40,20 +47,31 @@ pantalla(out,in,entrada);
                 msj(out,"N/A");
                 msj(out,eleccion);
                 String resultado = in.readLine();
-                                    System.out.println(resultado);
+                System.out.println(resultado);
+                int esperar = 10000;
                 int intentos = 0;
                 if(resultado.equals("UsrNC")){
+
                     System.out.println("Nombre de usuario o contraseña incorrecta");
+                    intentos++;
+                    System.out.println("numint "+intentos);
+                    System.out.println("segundos a esperar"+esperar);
+                    if(intentos % 3 == 0){
+                        System.out.println("Intentaste entrar muchas veces,por favor espera "+ esperar+" segundos");
+                        System.out.println("numint "+intentos);
+                        Thread.sleep(esperar);
+                       int newsperar = esperar + 10000;
+                       esperar = newsperar;
+                       System.out.println("segundos a esperar"+esperar);
+                    }
                 }
                 else if (resultado.equals("UsrSC")){
                     System.out.println("Bienvenido!");
+                    intentos = 0;
+                    esperar = 10000;
 
                 }
-        }
-        else if (eleccion.equals("2")){
-            pantreg(entrada,out);
-             msj(out,eleccion);
-        }
+    
     }
     static void pantreg(Scanner entrada, PrintWriter out){
            System.out.println("Nombre de usuario:");
--- El-Pistolero/src/main/java/com/mycompany/el/pistolero/Server/Coreapp.java
+++ El-Pistolero/src/main/java/com/mycompany/el/pistolero/Server/Coreapp.java
@@ -12,63 +12,58 @@ import java.sql.*;
 import java.io.*;  
 public class Coreapp {
 
-        public static void main(String[] args) throws Exception{
- Socket clientSocket = new Socket("127.0.0.1", 6666);
-PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8889/elpistolero","root","root");
-            System.out.println("###########SETUP TERMINADO, EMPEZANDO WHILE#############");
-while(true){
-Usuario usuarioent = new Usuario();
-usuarioent.setUsrname(in.readLine());
-usuarioent.setPassw(in.readLine());
-usuarioent.setEmail(in.readLine());
-usuarioent.setNombre(in.readLine());
-String eleccion = in.readLine();
-if(eleccion.equals("1")){
-if(LogUsuario(conn,usuarioent)==false){
-msj(out,"UsrNC");
-}
-else if (LogUsuario(conn,usuarioent)==true){
-msj(out,"UsrSC");
-}
-}
-else if (eleccion.equals("2")){
-    System.out.println(RegUsuario(conn,usuarioent));
-    System.out.println("RegUsuario");
-}
-            System.out.println("Eleccion="+eleccion);
- System.out.println("username: "+usuarioent.getUsrname());  
- System.out.println("passw: "+usuarioent.getPassw());
-System.out.println("Email: "+usuarioent.getEmail());
-System.out.println("nombre: "+usuarioent.getNombre());
-}
+public static void main(String[] args) throws Exception{
+    Socket clientSocket = new Socket("127.0.0.1", 6666);
+    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/elpistolero","root","root");
+    System.out.println("###########SETUP TERMINADO, EMPEZANDO WHILE#############");
+        while(true){
+            Usuario usuarioent = new Usuario();
+            usuarioent.setUsrname(in.readLine());
+            usuarioent.setPassw(in.readLine());
+            usuarioent.setEmail(in.readLine());
+            usuarioent.setNombre(in.readLine());
+            String eleccion = in.readLine();
+            if(eleccion.equals("1")){
+                if(logUsuario(conn,usuarioent)==false){
+                    msj(out,"UsrNC");
+                }
+                else if (logUsuario(conn,usuarioent)==true){
+                    msj(out,"UsrSC");
+                }
+            }
+            else if (eleccion.equals("2")){
+                regUsuario(conn,usuarioent);
+            }
         }
-            public static String RegUsuario(Connection conn,Usuario regusuario)throws Exception{
-Statement stmt  = conn.createStatement();
-        String sql = "INSERT INTO `Usuario`(`username`, `email`, `passw`, `nombre`)" +
-"VALUES ('"+regusuario.getUsrname()+"', '"+ regusuario.getEmail()+"', '"+ regusuario.getPassw()+"', '"+ regusuario.getNombre()+"');";
-                       System.out.println(sql);
-       stmt.execute(sql);
-       return("Registracion completa!");
     }
-        public static boolean LogUsuario(Connection conn, Usuario usuarioent)throws Exception{
-Statement stmt  = conn.createStatement();
-String sql = "SELECT `username`,`passw` FROM `Usuario`\n" +
-" WHERE username = '"+usuarioent.getUsrname()+"'" +
-"  AND passw = '"+usuarioent.getPassw()+"' ;";
-            System.out.println(sql);
-            ResultSet rs = stmt.executeQuery(sql);
-             if (rs.next() == false) {
-        System.out.println("Usuario no encontrado");
-return false;
-      } else {
-                 System.out.println("Usuario Encontrado");
- return true;
+
+public static void regUsuario(Connection conn,Usuario regusuario)throws Exception{
+    Statement stmt  = conn.createStatement();
+    String sql = "INSERT INTO `Usuario`(`username`, `email`, `passw`, `nombre`)" +
+    "VALUES ('"+regusuario.getUsrname()+"', '"+ regusuario.getEmail()+"', '"+ regusuario.getPassw()+"', '"+ regusuario.getNombre()+"');";
+    System.out.println(sql);
+    stmt.execute(sql);
+    }
+
+public static boolean logUsuario(Connection conn, Usuario usuarioent)throws Exception{
+    Statement stmt  = conn.createStatement();
+    String sql = "SELECT `username`,`passw` FROM `Usuario`\n" +
+    " WHERE username = '"+usuarioent.getUsrname()+"'" +
+    "  AND passw = '"+usuarioent.getPassw()+"' ;";
+    System.out.println(sql);
+    ResultSet rs = stmt.executeQuery(sql);
+    if (rs.next() == false){
+        return false;
+    }
+    else{
+        return true;
         }
-      }
-                     static void msj(PrintWriter out,String str){
+    }
+
+static void msj(PrintWriter out,String str){
      out.println(str);
-     }
+    }
   
 }
